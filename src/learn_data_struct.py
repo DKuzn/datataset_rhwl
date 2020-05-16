67ffbd8da0f5545053361aa8a868bfce51f27760
ivan-tretyak <das.blackya@gmail.com>
2020-05-16 09:15:09 +0300

    some patches. __str__, __add_data__. LearnData rewrite with dataclass. Add batch.
    Signed-off-by: ivan-tretyak <das.blackya@gmail.com>

--- src/learn_data_struct.py
+++ src/learn_data_struct.py
@@ -1,37 +1,51 @@
 import numpy as np
 from PIL import Image
+from dataclasses import dataclass
+import os
 
-
+@dataclass()
 class LearnData:
-    def __init__(self, letter, number, data):
-        self.letter: str = letter
-        self.number: int = number
-        self.data: np.ndarray = data
+    letter: str = None
+    data: np.ndarray = None
 
 
 class DataSet:
-    def __init__(self):
+    def __init__(self, batch_size):
         self.data_set = []
+        self.batch_size = batch_size
 
     def __str__(self):
-        current = self.data_set
-        out = "[" + str(current[0].data)
-        for i in range(1, len(self.data_set)):
-            current = self.data_set[i].data
-            out += ",\n" + str(current)
+        out = "["
+        for i in self.data_set:
+            out += ",\n" + 'letter:' + str(i.letter) + '\n' + str(i.data)
         return out + "]"
 
-    def add_data(self, path: str, letter: str, number: int):
-        self.data = Image.open(path)
-        self.np_data = np.array(self.data)
-        self.learn_data = LearnData(letter, number, self.np_data)
-        self.data_set.append(self.learn_data)
+    def __getitem__(self, item):
+        return self.data_set[item]
+
+    def add_data(self, path):
+        data = Image.open(path)
+        letter = os.path.splitext(os.path.basename(path))[0]
+        np_data = np.array(data)
+        learn_data = LearnData(letter, np_data)
+        self.data_set.append(learn_data)
 
     def show_data(self):  #метод под вопросом
         return self.data_set
 
+    def __get_batch_letter(self):
+        return np.random.randint(0, 62+1)
 
-dataset = DataSet()
-dataset.add_data("../cut_img/1/а.jpg", "а", 1)
-dataset.add_data("../cut_img/1/А.jpg", "А", 2)
-print(dataset)
+    def batch(self):
+        path = '../cut_img'
+        sub_folder = np.random.randint(1,8+1)
+        path = os.path.join(path, str(sub_folder))
+        list_image = os.listdir(path)
+        for i in range(self.batch_size):
+            self.add_data(os.path.join(path, list_image[self.__get_batch_letter()]))
+        return self.data_set
+
+
+a = DataSet(10)
+a.batch()
+print(a)
